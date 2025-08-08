5dac1e89b5f8514c158091aabb11bd3b162b78da
SundarMasetty <166877318+SundarMasetty@users.noreply.github.com>
2025-08-08 13:19:53 -0400

    create table commit

--- fact_data_modeling_1.sql
+++ fact_data_modeling_1.sql
@@ -5,3 +5,39 @@ SELECT * from bootcamp.nba_game_details LIMIT 10;
 SELECT DISTINCT(comment) from bootcamp.nba_game_details
 SELECT * from bootcamp.nba_game_details WHERE comment is NOT NULL ;
 SELECT * FROM bootcamp.nba_games;
+
+CREATE TABLE sundarmasetty.fct_nba_game_details(
+game_id BIGINT,
+Team_id BIGINT,
+player_id BIGINT,
+dim_team_abbreviation VARCHAR,
+dim_player_name VARCHAR,
+dim_start_position VARCHAR,
+dim_did_not_dress BOOLEAN,
+dim_not_with_team BOOLEAN,
+m_seconds_played  DOUBLE,
+m_fields_goals_made DOUBLE,
+m_field_goals_attempted DOUBLE,
+m_3_pointers_attempted DOUBLE,
+m_free_throws_made DOUBLE,
+m_free_throws_attempted DOUBLE,
+m_offensive_rebounds DOUBLE,
+m_defensive_rebounds DOUBLE,    -- using double can save the fractal part which will be helpful for the analysts
+m_rebounds DOUBLE,  
+m_assists DOUBLE,
+m_steals DOUBLE,
+m_blocks DOUBLE,
+m_turnovers DOUBLE,
+m_personal_fouls DOUBLE,
+m_points DOUBLE,
+m_plus_minus DOUBLE,
+dim_game_date DATE,
+dim_season INTEGER,
+dim_team_did_win BOOLEAN
+)
+WITH(
+Format = 'PARQUET',
+Partitioning = Array['dim_season']
+)
+
+
