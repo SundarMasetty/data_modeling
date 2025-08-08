69e4d64ed93dc5a94ad52d278458676301a5e424
SundarMasetty <166877318+SundarMasetty@users.noreply.github.com>
2025-08-08 16:23:06 -0400

    column names and cast changes commit

--- fact_data_modeling_1.sql
+++ fact_data_modeling_1.sql
@@ -52,4 +52,48 @@ Select game_id , season , home_team_wins, home_team_id , visitor_team_id from bo
 Select * from games g JOIN bootcamp.nba_game_details gd ON g.game_id = gd.game_id
 LIMIT 50;
 
+-- creating / making the data with the columns i need and changing the column names as per my conviction
+WITH games AS(
+Select game_id , game_date_est, season , home_team_wins, home_team_id , visitor_team_id from bootcamp.nba_games
+)
+Select 
+CAST(g.game_id  AS BIGINT) as game_id,
+gd.team_id,
+Gd.player_id,
+gd.team_abbreviation as dim_team_abbreviation,
+gd.player_name as dim_player_name,
+gd.start_position as dim_start_position,
+gd.comment LIKE '%DND%' AS dim_did_not_dress,
+gd.comment LIKE '%NWT%' AS dim_not_with_team,
+CASE WHEN CARDINALITY (SPLIT(min,':')) > 1 
+  THEN 
+CAST(CAST(SPLIT (min , ':')[1] AS DOUBLE ) * 60 + CAST(SPLIT(min,':')[2] AS DOUBLE ) AS INTEGER)
+ELSE
+  CAST(min as INTEGER)
+END
+AS 
+  seconds_played , -- conversion into seconds as in original table in the minutes which is not good for analytics 
+CAST(fgm as DOUBLE ) as m_fields_goals_made,
+CAST(fga as DOUBLE ) as m_fields_goals_attempted,
+CAST(fg3m as DOUBLE ) as m_3_pointers_made,
+CAST(fg3a as DOUBLE ) as m_3_pointers_attempted,
+CAST(ftm as DOUBLE ) as m_free_throws_made,
+CAST(fta as DOUBLE ) as m_free_throws_attempted,
+CAST(oreb AS DOUBLE) as m_offensive_rebounds,
+CAST(dreb AS DOUBLE ) as m_defensive_rebounds,
+CAST(reb as DOUBLE ) as m_rebounds,
+CAST(ast as DOUBLE ) as m_assists,
+CAST(stl as DOUBLE) as m_steals,
+CAST(blk as DOUBLE ) as m_blocks,
+CAST(turnovers AS DOUBLE ) as m_turnovers,  -- issue with to
+CAST(pf as DOUBLE ) as m_personal_fouls,
+CAST(plus_minus as DOUBLE ) as m_plus_minus,
+g.game_date_est AS dim_game_date,
+g.season as dim_season,
+CASE WHEN gd.team_id = g.home_team_id THEN home_team_wins = 1 
+  ELSE home_team_wins = 0 
+  END AS  
+  dim_team_win
+from games g JOIN bootcamp.nba_game_details gd ON g.game_id = gd.game_id
+
 
