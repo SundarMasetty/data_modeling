ba2886701c08a409914ae5647930f26355d7e14d
SundarMasetty <166877318+SundarMasetty@users.noreply.github.com>
2025-08-08 13:15:54 -0400

    Create fact_data_modeling_1.sql
    data exploration commit

---
+++ fact_data_modeling_1.sql
@@ -0,0 +1,7 @@
+SELECT * FROM bootcamp.nba_game_details limit 50
+SHOW CREATE TABLE bootcamp.nba_game_details
+SELECT count(1) From bootcamp.nba_game_details
+SELECT * from bootcamp.nba_game_details LIMIT 10;
+SELECT DISTINCT(comment) from bootcamp.nba_game_details
+SELECT * from bootcamp.nba_game_details WHERE comment is NOT NULL ;
+SELECT * FROM bootcamp.nba_games;
